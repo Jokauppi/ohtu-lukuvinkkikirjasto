2fbc113037de8fb1e4b8edea21407b261537e982
antonlep <antonleppa@gmail.com>
2021-11-25 20:32:39 +0200

    Create service that handles book tips

---
+++ src/entities/book_tip.py
@@ -0,0 +1,6 @@
+class BookTip:
+    def __init__(self, name, author, ISBN, publication_year):
+        self.name = name
+        self.author = author
+        self.ISBN = ISBN
+        self.publication_year = publication_year
---
+++ src/service.py
@@ -0,0 +1,12 @@
+from entities.book_tip import BookTip
+
+class Service:
+    def __init__(self, repository):
+        self._repository = repository
+
+    def create_book_tip(self, name, author, ISBN, publication_year):
+        book_tip = BookTip(name, author, ISBN, publication_year)
+        return self._repository.add(book_tip)
+
+    def get_all_book_tips(self):
+        return self._repository.get_all()
