536f96da5668bfa4feb2abea5b9132381535b902
tommijuslin <tommi.juslin@gmail.com>
2021-11-25 20:39:05 +0200

    add database_connection.py, initialise_database.py

---
+++ src/database_connection.py
@@ -0,0 +1,9 @@
+import os
+import sqlite3
+
+
+connection = sqlite3.connect(os.getenv('DATABASE'))
+connection.row_factory = sqlite3.Row
+
+def get_connection():
+    return connection
---
+++ src/initialise_database.py
@@ -0,0 +1,34 @@
+from database_connection import get_connection
+
+
+def drop_tables(connection):
+    c = connection.cursor()
+
+    c.execute("""
+        DROP TABLE IF EXISTS books;
+    """)
+
+    connection.commit()
+
+def create_tables(connection):
+    c = connection.cursor()
+
+    c.execute("""
+        CREATE TABLE books (
+            title TEXT,
+            author TEXT,
+            isbn TEXT,
+            publication_year INTEGER
+        );
+    """)
+
+    connection.commit()
+
+def initialise_database():
+    connection = get_connection()
+
+    drop_tables(connection)
+    create_tables(connection)
+
+if __name__ == "__main__":
+    initialise_database()
