0cb8a9add30e6a4e01464f9f3fe394524e582598
tommijuslin <tommi.juslin@gmail.com>
2021-11-26 10:29:51 +0200

    add book_tip_repository

---
+++ src/repositories/book_tip_repository.py
@@ -0,0 +1,34 @@
+from src.database_connection import get_connection
+from entities.book_tip import BookTip
+
+
+class BookTipRepository:
+    def __init__(self, connection):
+        self._connection = connection
+
+    def add(self, book_tip):
+        cursor = self._connection.cursor()
+
+        cursor.execute("SELECT * FROM BookTips WHERE isbn=?", (book_tip.isbn,))
+
+        result = cursor.fetchone()
+
+        if result:
+            return
+        else:
+            cursor.execute("INSERT INTO BookTips VALUES (?, ?, ?, ?)",
+                (book_tip.name, book_tip.author, book_tip.isbn, book_tip.publication_year))
+
+            self._connection.commit()
+
+    def get_all(self):
+        cursor = self._connection.cursor()
+
+        cursor.execute("SELECT * FROM BookTips")
+
+        rows = cursor.fetchall()
+
+        return [BookTip(row["name"], row["author"],row["isbn"], row["publication_year"]) for row in rows]
+
+
+book_tip_repository = BookTipRepository(get_connection())
