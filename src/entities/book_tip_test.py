46996ff8af9e6f8e7878bd5d1a4555fbc01fd4c3
JonathanHeyno <jona.heyno@gmail.com>
2021-11-26 02:39:11 +0200

    initial commit

---
+++ src/entities/book_tip_test.py
@@ -0,0 +1,19 @@
+import unittest
+from book_tip import BookTip
+
+
+class TestBookTip(unittest.TestCase):
+    def setUp(self):
+        self.booktip = BookTip("name", "author", "9781107009462", "2021")
+
+    def test_getName(self):
+        self.assertEqual(self.booktip.getName(), "name")
+
+    def test_getAuthor(self):
+        self.assertEqual(self.booktip.getAuthor(), "author")
+    
+    def test_getISBN(self):
+        self.assertEqual(self.booktip.getISBN(), "9781107009462")
+    
+    def test_getPublicationYear(self):
+        self.assertEqual(self.booktip.getPublicationYear(), "2021")
