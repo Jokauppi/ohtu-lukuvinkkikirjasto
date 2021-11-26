21f90898b63b83937d075981a945e876aada1892
antonlep <antonleppa@gmail.com>
2021-11-26 12:08:45 +0200

    Add get methods to book tip

--- src/entities/book_tip.py
+++ src/entities/book_tip.py
@@ -1,6 +1,18 @@
 class BookTip:
     def __init__(self, name, author, ISBN, publication_year):
-        self.name = name
-        self.author = author
-        self.ISBN = ISBN
-        self.publication_year = publication_year
+        self._name = name
+        self._author = author
+        self._ISBN = ISBN
+        self._publication_year = publication_year
+
+    def getName(self):
+        return self.name
+
+    def getAuthor(self):
+        return self.author
+
+    def getISBN(self):
+        return self.ISBN
+
+    def getPublicationYear(self):
+        return self.publication_year()
