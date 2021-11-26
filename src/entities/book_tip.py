98100e1445b41326d915d8363ac1d101ccd44dbb
antonlep <antonleppa@gmail.com>
2021-11-26 12:39:37 +0200

    Add properties and str method

--- src/entities/book_tip.py
+++ src/entities/book_tip.py
@@ -1,18 +1,28 @@
 class BookTip:
-    def __init__(self, name, author, ISBN, publication_year):
+    def __init__(self, name: str, author: str, ISBN: str, publication_year: str):
         self._name = name
         self._author = author
         self._ISBN = ISBN
         self._publication_year = publication_year
 
-    def getName(self):
+    @property
+    def name(self):
         return self._name
 
-    def getAuthor(self):
+    @property
+    def author(self):
         return self._author
 
-    def getISBN(self):
+    @property
+    def ISBN(self):
         return self._ISBN
 
-    def getPublicationYear(self):
+    @property
+    def publication_year(self):
         return self._publication_year
+
+    def __str__(self):
+        return f"Book name: {self._name}\n" \
+                f"Author: {self._author}\n" \
+                f"ISBN: {self._ISBN}\n" \
+                f"Publication year: {self._publication_year}"
