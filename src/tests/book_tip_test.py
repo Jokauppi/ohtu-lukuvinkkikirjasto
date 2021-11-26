eef63a7772312016aa79fa203ef5f16f5b0f33a8
tommijuslin <tommi.juslin@gmail.com>
2021-11-26 18:55:09 +0200

    fix tests

--- src/tests/book_tip_test.py
+++ src/tests/book_tip_test.py
@@ -7,13 +7,13 @@ class TestBookTip(unittest.TestCase):
         self.booktip = BookTip("name", "author", "9781107009462", "2021")
 
     def test_getName(self):
-        self.assertEqual(self.booktip.getName(), "name")
+        self.assertEqual(self.booktip.name, "name")
 
     def test_getAuthor(self):
-        self.assertEqual(self.booktip.getAuthor(), "author")
+        self.assertEqual(self.booktip.author, "author")
     
     def test_getISBN(self):
-        self.assertEqual(self.booktip.getISBN(), "9781107009462")
+        self.assertEqual(self.booktip.isbn, "9781107009462")
     
     def test_getPublicationYear(self):
-        self.assertEqual(self.booktip.getPublicationYear(), "2021")
+        self.assertEqual(self.booktip.publication_year, "2021")
