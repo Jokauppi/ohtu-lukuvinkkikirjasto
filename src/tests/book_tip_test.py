bb935c0fb2235a744a353c4e02a73be0f5c9da70
JonathanHeyno <jona.heyno@gmail.com>
2021-11-26 20:30:04 +0200

    Added tests for changeing values

--- src/tests/book_tip_test.py
+++ src/tests/book_tip_test.py
@@ -17,3 +17,19 @@ class TestBookTip(unittest.TestCase):
     
     def test_getPublicationYear(self):
         self.assertEqual(self.booktip.publication_year, "2021")
+
+    def test_changeName(self):
+        self.booktip.name = "newName of nook version 2.2"
+        self.assertEqual(self.booktip.name, "newName of nook version 2.2")
+
+    def test_changeAuthor(self):
+        self.booktip.author = "Firstname, M. Lastname & Otherguy McDonald"
+        self.assertEqual(self.booktip.author, "Firstname, M. Lastname & Otherguy McDonald")
+    
+    def test_changeISBN(self):
+        self.booktip.isbn = "11111-241548"
+        self.assertEqual(self.booktip.isbn, "11111-241548")
+    
+    def test_changePublicationYear(self):
+        self.booktip.publication_year = "2022"
+        self.assertEqual(self.booktip.publication_year, "2022")
