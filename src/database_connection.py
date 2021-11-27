b400719af4c450be921cdb3cc80dd0887920164d
JonathanHeyno <jona.heyno@gmail.com>
2021-11-27 16:59:54 +0200

    configured environment variables

--- src/database_connection.py
+++ src/database_connection.py
@@ -1,9 +1,13 @@
 import os
 import sqlite3
+from config import DATABASE
+
 
 def get_connection(db_file=os.getenv('DATABASE')):
 
+
     connection = sqlite3.connect(db_file)
     connection.row_factory = sqlite3.Row
+
 
     return connection
