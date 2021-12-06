fdd00350fef7c8c26826e682969c6eebae05ac46
antonlep <antonleppa@gmail.com>
2021-12-06 15:35:34 +0200

    add unittests for service

--- src/tests/service_test.py
+++ src/tests/service_test.py
@@ -11,12 +11,17 @@ class MockBookTipRepository:
     def get_all(self):
         return self.booktips
 
-    
+    def mark_as_read(self, id):
+        self.booktips[id-1].read = True
 
     def add(self, booktip):
         self.booktips.append(booktip)
 
         return booktip
+
+    def get_read(self, read):
+        if read:
+            return[book for book in self.booktips if book.read == True]
 
 class MockBlogTipRepository:
     def __init__(self, blogtips=None):
@@ -30,6 +35,13 @@ class MockBlogTipRepository:
 
         return blogtip
 
+    def mark_as_read(self, id):
+        self.blogtips[id-1].read = True
+
+    def get_read(self, read):
+        if read:
+            return[blog for blog in self.blogtips if blog.read == True]
+
 class MockVideoTipRepository:
     def __init__(self, videotips=None):
         self.videotips = videotips or []
@@ -42,18 +54,25 @@ class MockVideoTipRepository:
 
         return videotip
 
+    def mark_as_read(self, id):
+        self.videotips[id-1].read = True
+
+    def get_read(self, read):
+        if read:
+            return[video for video in self.videotips if video.read == True]
+
 class TestService(unittest.TestCase):
     def setUp(self):
         self.service = Service(MockBookTipRepository(), MockBlogTipRepository(), MockVideoTipRepository())
 
-        self.booktip_a = BookTip('Book1', 'Firstname1, lastname1', '1234', '2001')
-        self.booktip_b = BookTip('Book2', 'Firstname2, lastname2', '1234', '2002')
+        self.booktip_a = BookTip('Book1', 'Firstname1, lastname1', '1234', '2001', 1, False)
+        self.booktip_b = BookTip('Book2', 'Firstname2, lastname2', '1234', '2002', 2, False)
 
-        self.blogtip_a = BlogTip('Blog1', 'Firstname1, lastname1', 'www.1.com')
-        self.blogtip_b = BlogTip('Blog2', 'Firstname2, lastname2', 'www.2.com')
+        self.blogtip_a = BlogTip('Blog1', 'Firstname1, lastname1', 'www.1.com', 1, False)
+        self.blogtip_b = BlogTip('Blog2', 'Firstname2, lastname2', 'www.2.com', 2, False)
     
-        self.videotip_a = VideoTip('Video1', 'www.video.com/1')
-        self.videotip_b = VideoTip('VIdeo2', 'www.video.com/2')
+        self.videotip_a = VideoTip('Video1', 'www.video.com/1', 1, False)
+        self.videotip_b = VideoTip('VIdeo2', 'www.video.com/2', 2, False)
 
 # Book Tips
 
@@ -64,7 +83,18 @@ class TestService(unittest.TestCase):
 
         self.assertEqual(len(booktips), 1)
         self.assertEqual(booktips[0].__str__(), self.booktip_a.__str__())
-    
+
+
+    def test_mark_book_tip_as_read(self):
+
+        self.service.create_book_tip(self.booktip_a.name, self.booktip_a.author, self.booktip_a.isbn, self.booktip_a.publication_year)
+        self.service.create_book_tip(self.booktip_b.name, self.booktip_b.author, self.booktip_b.isbn, self.booktip_b.publication_year)
+
+        self.service.mark_book_tip_as_read(1)
+        booktips = self.service.get_read_book_tips(True)
+
+        self.assertEqual(len(booktips), 1)
+        self.assertEqual(booktips[0].__str__(), self.booktip_a.__str__())
     
     def test_get_all_book_tips(self):
 
@@ -86,7 +116,18 @@ class TestService(unittest.TestCase):
 
         self.assertEqual(len(videotips), 1)
         self.assertEqual(videotips[0].__str__(), self.videotip_a.__str__())
-    
+
+
+    def test_mark_video_tip_as_read(self):
+
+        self.service.create_video_tip(self.videotip_a.title, self.videotip_a.url)
+        self.service.create_video_tip(self.videotip_b.title, self.videotip_b.url)
+
+        self.service.mark_video_tip_as_read(1)
+        videotips = self.service.get_read_video_tips(True)
+
+        self.assertEqual(len(videotips), 1)
+        self.assertEqual(videotips[0].__str__(), self.videotip_a.__str__())
     
     def test_get_all_video_tips(self):
 
@@ -109,7 +150,18 @@ class TestService(unittest.TestCase):
         self.assertEqual(len(blogtips), 1)
         self.assertEqual(blogtips[0].__str__(), self.blogtip_a.__str__())
     
-    
+    def test_mark_blog_tip_as_read(self):
+
+        self.service.create_blog_tip(self.blogtip_a.name, self.blogtip_a.author, self.blogtip_a.url)
+        self.service.create_blog_tip(self.blogtip_b.name, self.blogtip_b.author, self.blogtip_b.url)
+
+        self.service.mark_blog_tip_as_read(1)
+        blogtips = self.service.get_read_blog_tips(True)
+
+        self.assertEqual(len(blogtips), 1)
+        self.assertEqual(blogtips[0].__str__(), self.blogtip_a.__str__())
+
+
     def test_get_all_blog_tips(self):
 
         self.service.create_blog_tip(self.blogtip_a.name, self.blogtip_a.author, self.blogtip_a.url)
