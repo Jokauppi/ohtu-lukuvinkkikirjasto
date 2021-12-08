70ef080c6598377cee89cdd84cb4544fc03e767e
Joona Kauppila <joona.kauppila@helsinki.fi>
2021-12-08 09:58:28 +0200

    fix .env path in config

--- lukuvinkkikirjasto/config.py
+++ lukuvinkkikirjasto/config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 dirname = os.path.dirname(__file__)
 
 try:
-    #print(os.path.join(dirname, '..', '.env'))
-    load_dotenv(dotenv_path=os.path.join(dirname, '..', '.env'))
+    #print(os.path.join(dirname, '.env'))
+    load_dotenv(dotenv_path=os.path.join(dirname, '.env'))
 except FileNotFoundError:
     pass
 
